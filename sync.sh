43a43aaf9277971d4c90764c219d8e3221840264
lubshad <lubshad4u4@gmail.com>
2026-03-31 13:40:18 +0530

    scripts added for deployments

---
+++ sync.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SERVER="frappe@mcalfrappe.coreaxissolutions.in"
+IDENTITY_FILE="personal"
+REMOTE_DIR="/home/frappe/frappe-bench/apps/exam"
+
+echo "Starting sync to $SERVER..."
+
+rsync -avz \
+  --exclude '.git' \
+  --exclude '__pycache__' \
+  --exclude '*.pyc' \
+  --exclude '.DS_Store' \
+  -e "ssh -i $IDENTITY_FILE" \
+  ./ $SERVER:$REMOTE_DIR/
+
+echo "Sync completed! Running bench migrate..."
+
+ssh -i $IDENTITY_FILE $SERVER "
+cd /home/frappe/frappe-bench
+./env/bin/pip install -r apps/exam/requirements.txt || true
+bench --site mcalfrappe.coreaxissolutions.in migrate
+bench restart
+"
+
+echo "Deployment successful!"
